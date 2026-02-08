622a988525ac49f5658f43421061208eb01ed82c
modharya1-dotcom <modharya1@gmail.com>
2026-02-08 13:14:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const AI_URL = "https://7f8a-2401.ngrok-free.app";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
